684d10626da93b6e00ba8b026870ab9b8559ce1e
Bidhan Baral <bbaral2062@gmail.com>
2025-02-25 13:30:48 -0500

    Completed the menu and about tabs

--- src/about.js
+++ src/about.js
@@ -1,5 +1,15 @@
 const content = document.querySelector("#content");
 
 export function aboutContent() {
-    
+    const heading = document.createElement("h2");
+    const address = document.createElement("p");
+    const description = document.createElement("p");
+
+    heading.textContent = "Paurabi's Mission";
+    description.textContent = "Paurabi's Mission is to make people laugh by feeding them laughing."
+    address.textContent = "321 Japan Road, Korea Town, China State, North Korea, 23424-2342";
+
+    content.appendChild(heading);
+    content.appendChild(description);
+    content.appendChild(address);
 }
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,31 @@
 const content = document.querySelector("#content");
 
 export function menuContent() {
-    
+    const item1 = document.createElement("h3");
+    const item1description = document.createElement("p");
+    const item2 = document.createElement("h3");
+    const item2description = document.createElement("p");
+    const item3 = document.createElement("h3");
+    const item3description = document.createElement("p");
+    const item4 = document.createElement("h3");
+    const item4description = document.createElement("p");
+
+    item1.textContent = "Paurabi's Spicy Chicken";
+    item2.texteContent = "Mata Ji's sour Gundruk";
+    item3.textContent = "Paurabi's Laughing";
+    item4.textContent = "Puntey's Pustakari";
+
+    item1description = "";
+    item2description = "";
+    item3description = "";
+    item4description = "";
+
+    content.appendChild(item1);
+    content.appendChild(item1description);
+    content.appendChild(item2);
+    content.appendChild(item2description);
+    content.appendChild(item3);
+    content.appendChild(item3description);
+    content.appendChild(item4);
+    content.appendChild(item4description);
 }
